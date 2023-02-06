8fe41d402fa4be3eeaaa0f96eed3e87967e9c969
Esin Andrew <kubanesin@gmail.com>
2023-02-06 20:42:23 +0300

    feat:реализация кликов по категориям пицц

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
+import React from 'react';
 
 const Categories = () => {
-  return (
-   <div class="categories">
-   <ul>
-      <li className="active">Все</li>
-      <li>Мясные</li>
-      <li>Вегетарианская</li>
-      <li>Гриль</li>
-      <li>Острые</li>
-      <li>Закрытые</li>
-   </ul>
-</div>
-  )
+
+   // Состояние индекса выбора типа сортировки
+   const [indexSort, setIndexSort] = React.useState(0)
+
+   // Создал массив категорий пицц
+   const categorArray = [
+      'Все',
+      'Мясные',
+      'Вегетарианская',
+      'Гриль',
+      'Острые',
+      'Закрытые',
+   ];
+
+   return (
+      <div class="categories">
+         <ul>
+
+            {categorArray.map((item, i) =>
+               <li onClick={()=>setIndexSort(i)} className={indexSort === i ? 'active' : ''} key={item}>{item}</li>
+            )
+            }
+            
+         </ul>
+      </div>
+   )
 }
 
 export default Categories
