1f26c2b0f3201ad96bc0de20f7311b644ff6d207
Esin Andrew <kubanesin@gmail.com>
2023-03-19 16:13:04 +0300

    feat:начал применять TS к проекту

--- src/components/FullPizza/FullPizza.tsx
+++ src/components/FullPizza/FullPizza.tsx
@@ -2,10 +2,14 @@ import axios from 'axios';
 import React from 'react'
 import { useParams } from 'react-router-dom';
 
-const FullPizza = () => {
+const FullPizza:React.FC = () => {
 
    // Состояние пиццы
-   const [pizza, setPizza] = React.useState();
+   const [pizza, setPizza] = React.useState<{
+      title: string;
+      imageUrl: string;
+      price: number;
+   }>();
 
 
    // Воспользовался Хуком useParams чтобы вытащить путь
@@ -24,7 +28,7 @@ const FullPizza = () => {
 
    // В состоянии у меня пусто, поэтому пока идет запрос нужно сделать условие
    if (!pizza) {
-      return 'Идет загрузка пиццы..'
+      return <>Идет загрузка пиццы..</>
    }
 
    return (
