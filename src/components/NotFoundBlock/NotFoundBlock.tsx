9daa7a7a00e8e73dbb787911e5810f8143c5e73b
Esin Andrew <kubanesin@gmail.com>
2023-04-06 20:40:55 +0300

    fix:кнопку в notFoundBlock

--- src/components/NotFoundBlock/NotFoundBlock.tsx
+++ src/components/NotFoundBlock/NotFoundBlock.tsx
@@ -1,25 +1,26 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 import classes from './NotFoundBlock.module.scss';
 
 const NotFoundBlock: React.FC = () => {
-   return (
-
+  return (
+    <div className={classes.notFound}>
       <div className={classes.root}>
-         <h1>
-            <span>😕</span>
-            <br />
-            Ничего не найдено.
-         </h1>
-         <p className={classes.description}>Данная страница отсутствует в нашем магазине </p>
-         <Link to="/" className="button button--black">
-               <span>Вернуться назад</span>
-            </Link>
+        <h1>
+          <span>😕</span>
+          <br />
+          Ничего не найдено
+        </h1>
+        <p className={classes.description}>Данная страница отсутствует в нашем магазине </p>
       </div>
+      <div className={classes.notFoundBtn}>
+        <Link to="/" className="button button--black">
+          <span>Вернуться назад</span>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-
-   )
-}
-
-export default NotFoundBlock
+export default NotFoundBlock;
